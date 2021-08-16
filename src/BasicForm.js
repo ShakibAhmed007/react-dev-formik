847563937afcecee60476dda386ac83dfa9a9c76
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-08-16 12:40:16 +0600

    1 file modified

--- src/BasicForm.js
+++ src/BasicForm.js
@@ -1,6 +1,30 @@
 import React from 'react';
 import { Formik, useFormik } from 'formik';
 
+// validation
+const validate = values => {
+  const errors = {};
+  if (!values.firstName) {
+    errors.firstName = 'Required';
+  } else if (values.firstName.length > 15) {
+    errors.firstName = 'Must be 15 characters or less';
+  }
+
+  if (!values.lastName) {
+    errors.lastName = 'Required';
+  } else if (values.lastName.length > 20) {
+    errors.lastName = 'Must be 20 characters or less';
+  }
+
+  if (!values.email) {
+    errors.email = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address';
+  }
+
+  return errors;
+};
+
 const BasicForm = () => {
   const formik = useFormik({
     initialValues: {
@@ -8,6 +32,7 @@ const BasicForm = () => {
       lastName: '',
       email: ''
     },
+    validate,
     onSubmit: values => {
       alert(JSON.stringify(values, null, 2));
     }
@@ -22,6 +47,7 @@ const BasicForm = () => {
         onChange={formik.handleChange}
         value={formik.values.firstName}
       />
+      {formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
       <br />
       <label htmlFor="lastName">Last Name</label>
       <input
@@ -31,6 +57,7 @@ const BasicForm = () => {
         onChange={formik.handleChange}
         value={formik.values.lastName}
       />
+      {formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
       <br />
       <label htmlFor="email">Email Address</label>
       <input
@@ -40,6 +67,7 @@ const BasicForm = () => {
         onChange={formik.handleChange}
         value={formik.values.email}
       />
+      {formik.errors.email ? <div>{formik.errors.email}</div> : null}
       <br />
       <button type="submit">Submit</button>
     </form>

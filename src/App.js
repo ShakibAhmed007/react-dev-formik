2fe3c23fc96b7408ae90cce35469813d365c661d
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-08-16 12:46:52 +0600

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './style.css';
 import BasicForm from './BasicForm';
+import BasicFormValidationByYup from './BasicFormValidationByYup';
 
 export default function App() {
   return (
     <div>
       <h1>Formik Example</h1>
       <BasicForm />
+      <hr />
+      <h1>Formik with Yup</h1>
+      <BasicFormValidationByYup />
     </div>
   );
 }
